a824bc5fd48f23d20327789f2e86250b438f3680
saad_ <saadeid@cmail.carleton.ca>
2022-12-08 15:05:40 -0500

    Adding SQL files to Project

---
+++ SQL/SampleData.sql
@@ -0,0 +1,14 @@
+insert into publisher values ( 'author1', '123456', 'author1@email.com', '123 fake st', '6134567890', '1234567');
+insert into book_data values ('book1', 'author1', 'drama');
+insert into book values ('123456', 'book1', '123', '25', '123456', '0', '10');
+insert into inventory values ('123456', 'book1', '5', '1');
+
+insert into publisher values ( 'author2', '1234568', 'author2@email.com', '124 fake st', '6134567891', '1234568');
+insert into book_data values ('book2', 'author2', 'drama');
+insert into book values ('123457', 'book2', '123', '25','1234568', '0', '10');
+insert into inventory values ('123457', 'book2', '5', '1');
+
+insert into publisher values ( 'author3', '1234569', 'author3@email.com', '125 fake st', '6134567892', '1234569');
+insert into book_data values ('book3', 'author3', 'drama');
+insert into book values ('123458', 'book3', '123', '25', '1234569','0', '10');
+insert into inventory values ('123458', 'book3', '5', '1');
